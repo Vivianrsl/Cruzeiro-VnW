f54fcc78b432703f6a0d7f096d58982b70270ae2
Vivian Sanchez <svivian949@gmail.com>
2023-12-06 15:17:11 -0300

    realizando tarefa

---
+++ script.js
@@ -0,0 +1,11 @@
+console.log('Ano novo está chegando!!')
+let cantora = 'Mariah Carey'
+let idade = 54
+let anoAtual = 2023
+anoAtual -= idade
+
+let dataNascimento = 1969
+console.log(dataNascimento)
+
+let quartafeira = true
+console.log(typeof quartaFeira)
