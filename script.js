0b1ef071c384e53390724fd193c84a270734a347
Vivian Sanchez <svivian949@gmail.com>
2023-12-08 20:26:53 -0300

    tarefa finalizada

--- script.js
+++ script.js
@@ -9,3 +9,17 @@ console.log(dataNascimento)
 
 let quartafeira = true
 console.log(typeof quartaFeira)
+
+let _idade = 31;
+if (_idade >= 18) {
+    console.log('Poderá curtir o show');
+} else {
+    console.log('Sinto muito, mas assistirá da TV Globo');
+}
+
+let aniversário = 'Abril'
+if (aniversário === 'Janeiro' || aniversário == 'Fevereiro' ||  aniversário == 'Março') {
+    console.log('Uma ou mais opções estão corretas. O mês escolhido foi o mês ${aniversário}')
+} else {
+    console.log('Algo de errado não está certo. o mês digitado foi o mês de ${aniversário}');
+}
